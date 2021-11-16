e6996354c15c2ad3606a5d0d7d1d3afb17c67739
Guan <1105793391@qq.com>
2021-11-16 16:50:28 +0800

    <!--Ribbon随机负载规则-->

--- cloud-consumer-order80/src/main/java/com/angenin/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/angenin/springcloud/OrderMain80.java
@@ -1,8 +1,10 @@
 package com.angenin.springcloud;
 
+import com.angenin.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -11,6 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MySelfRule.class)
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
--- cloud-consumer-order80/src/main/java/com/angenin/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/angenin/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.angenin.springcloud.controller;
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +39,18 @@ public class OrderController {
         log.info("********查询的id：" + id);
         //getForObject两个参数：请求地址，返回的对象类型
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+    @GetMapping("/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id){
+        ResponseEntity<CommonResult> entity=restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        log.info(entity.getStatusCode()+"\t"+entity.getHeaders());
+
+        if (entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }else {
+            return new CommonResult<>(444,"操作失败");
+        }
     }
 }
 
